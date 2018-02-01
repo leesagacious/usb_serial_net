7c87e73390c6980888a5baf104f1aa3eaf16e26c
leesagacious <sensor1010@163.com>
2018-02-02 00:06:41 +0800

    Update lee_usb_wwan_write.c

--- lee_usb_wwan_write.c
+++ lee_usb_wwan_write.c
@@ -1,5 +1,20 @@
 int lee_usb_wwan_write()
 {
-
+  /*
+    1 : Get the private data structure maintained by the port
+  */
+  
+  /*
+    2 : Get the output urb maintained by this private data structure
+  */
+  
+  /*
+    3 : Copy the data to be sent to the urb
+  */
+  
+  /*
+    4 : Submit the urb
+  */
+  
   return count;
 }
